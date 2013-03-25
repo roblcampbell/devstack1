2da295208ce937984fe16f7407e222c9ec93ce7c
unknown <rcampbell@PLYWS11262.DataRecognitionCorp.com>
2013-03-25 15:54:24 -0500

    First commit of git sandbox
    Added server and client packages. Text file.

---
+++ com/rob/gitsandbox/client/Client.java
@@ -0,0 +1,7 @@
+package com.rob.gitsandbox.client;
+
+public class Client {
+  public static void main(String[] args) {
+    System.out.println("Client stub");
+  }
+}
---
+++ com/rob/gitsandbox/server/Server.java
@@ -0,0 +1,7 @@
+package com.rob.gitsandbox.server;
+
+public class Server {
+  public static void main(String[] args) {
+    System.out.println("Server stub");
+  }
+}
